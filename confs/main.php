4a49dc1cd4800d517d4f34d2b4b5b46acf422771
Rrayor <simon.benjamin93@gmail.com>
2017-02-13 14:31:53 +0100

    fix #3

--- confs/main.php
+++ confs/main.php
@@ -6,11 +6,8 @@ define('LIB_PATH', 'Libs/');
 define('MODEL_PATH', 'Model/');
 define('VIEW_PATH', 'View/');
 define('JS_PATH', 'Public/JS/');
-define('CSS_PATH', 'Public/CSS/');
+define('CSS_PATH', '../../Public/CSS/');
 define('IMAGE_PATH', 'Public/IMAGES/');
 $errorMessage = '';
 $background = '/' . IMAGE_PATH . 'SYSTEM/bg.png';
-$cssFileNames = ['bootstrap.min', 'menu-style', 'content-style'];
-$menuPoints = [['name' => 'Home', 'link' => 'index', 'active' => 'true'],
-    ['name' => 'About Me', 'link' => 'aboutme', 'active' => 'true']
-];
+$cssFileNames = ['main'];
